f600d2597623f48f838191f5ae102b9998061094
Anup Kumar Aditya <anupkumaraditya123@gmail.com>
2017-08-04 12:33:48 +0530

    spy name and salutation

--- spychat/main.py
+++ spychat/main.py
@@ -1,1 +1,5 @@
-print 'hello'
+print 'Let\'s get Started...'
+spy_name = raw_input('What is you Name ? : ')
+spy_salutation = raw_input('What should we call you?')
+spy_name = spy_salutation + ' ' + spy_name
+print 'Welcome ' + spy_name + ' Glad to have you back with us.'
