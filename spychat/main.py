a2e8ff2a71eff1a026e255c128d06be10ce86e22
Anup Kumar Aditya <anupkumaraditya123@gmail.com>
2017-08-09 11:45:50 +0530

    if condition used

--- spychat/main.py
+++ spychat/main.py
@@ -1,5 +1,7 @@
 print 'Let\'s get Started...'
 spy_name = raw_input('What is you Name ? : ')
-spy_salutation = raw_input('What should we call you?')
-spy_name = spy_salutation + ' ' + spy_name
-print 'Welcome ' + spy_name + ' Glad to have you back with us.'
+if len(spy_name) > 0 :
+    #logic will be here if condition is true
+    spy_salutation = raw_input('What should we call you?')
+    spy_name = spy_salutation + ' ' + spy_name
+    print 'Welcome ' + spy_name + ' Glad to have you back with us.'
